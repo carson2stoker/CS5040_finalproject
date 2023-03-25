111b1817aaf1da5f33c50d8d943690f2f35cf749
Maximas5 <rabidyahoo1@gmail.com>
2023-03-25 10:55:20 -0600

    Fixed bugs in the scraper

--- utilities/chatGPTscraper.py
+++ utilities/chatGPTscraper.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
+start_link = 69
+
 # URL of the website to scrape
 url = 'https://oceans11.lanl.gov/deepwaterimpact/yC11_300x300x300-FourScalars_resolution.html'
 
@@ -14,9 +16,9 @@ soup = BeautifulSoup(html_content, 'html.parser')
 
 # Find all the links on the webpage
 links = soup.find_all('a')
-links = links[:len(links) - 1]
+links = links[start_link:len(links) - 1]
 
-file_number = 0
+file_number = start_link
 
 for link in links:
     # Get the URL of the link
@@ -32,4 +34,4 @@ for link in links:
     with open(file_name, 'wb') as f:
         f.write(link_html_content)
         file_number += 1
-        print(f'File {str(file_number)} of {len(links)} written to: {file_name}')
+        print(f'File {str(file_number)} of {len(links) + start_link} written to: {file_name}')
