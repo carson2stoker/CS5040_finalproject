c764beeadf6d889cda1a18b227c84a74c3c48388
Maximas5 <rabidyahoo1@gmail.com>
2023-03-31 03:20:34 -0600

    Updated scraper to be more usable

--- utilities/chatGPTscraper.py
+++ utilities/chatGPTscraper.py
@@ -2,49 +2,65 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-start_link = 1
-minimize_scalar = 10
+def get_data(url, start=0, scalar=0, path=-1):
+    if path == -1:
+        path = os.getcwd() + '\\' + url.split('/')[-1].split('_')[0]
 
-# URL of the website to scrape
-url = 'https://oceans11.lanl.gov/deepwaterimpact/yA31_300x300x300-AllScalars_resolution.html'
+    if scalar != 0:
+        path += f"_sc{scalar}"
 
-# Make a request to the URL and get the HTML content
-response = requests.get(url)
-html_content = response.content
-
-# Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(html_content, 'html.parser')
-
-# Find all the links on the webpage
-all_links = soup.find_all('a')
-
-file_counter = 0
-links = []
-for link_index in range(len(all_links)):
-    if file_counter == 0:
-        links.append(all_links[link_index])
-    if file_counter == minimize_scalar - 1:
-        file_counter = 0
+    # Create folder for data
+    try:
+        os.mkdir(path)
+    except FileExistsError:
+        print("File already exists")
     else:
-        file_counter += 1
+        print(f"Folder created at {path}")
+    print(f"Data will be located here: {path}")
 
-links = links[start_link:len(links) - 1]
 
+    # Make a request to the URL and get the HTML content
+    response = requests.get(url)
+    html_content = response.content
 
-file_number = start_link
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
 
-for link in links:
-    # Get the URL of the link
-    link_url = link.get('href')
-    print(f'Downloading...')
-    
-    # Make a request to the link URL and get the HTML content
-    link_response = requests.get(link_url)
-    link_html_content = link_response.content
-    
-    # Save the HTML content to a file with a .vtk extension
-    file_name = os.getcwd() + '\\data_scalar_partial\\' + link_url.split('/')[-1]
-    with open(file_name, 'wb') as f:
-        f.write(link_html_content)
-        file_number += 1
-        print(f'File {str(file_number)} of {len(links) + start_link} written to: {file_name}')
+    # Find all the links on the webpage
+    all_links = soup.find_all('a')
+
+    # Remove the first and last link (they don't have data)
+    links = links[1:len(links) - 1]
+
+    # Apply scalar
+    if scalar != 0:
+        file_counter = 0
+        links = []
+        for link_index in range(len(all_links)):
+            if file_counter == 0:
+                links.append(all_links[link_index])
+            if file_counter == scalar - 1:
+                file_counter = 0
+            else:
+                file_counter += 1
+
+    file_number = start
+    for link in links:
+        # Get the URL of the link
+        link_url = link.get('href')
+        print(f'Downloading...')
+        
+        # Make a request to the link URL and get the HTML content
+        link_response = requests.get(link_url)
+        link_html_content = link_response.content
+        
+        # Save the HTML content to a file with a .vtk extension
+        file_name = path + '\\' + link_url.split('/')[-1]
+        with open(file_name, 'wb') as f:
+            f.write(link_html_content)
+            file_number += 1
+            print(f'File {str(file_number)} of {len(links) + start} written to: {file_name}')
+
+if __name__ == "__main__":
+    url = 'https://oceans11.lanl.gov/deepwaterimpact/yA31_300x300x300-AllScalars_resolution.html'
+    get_data(url, scalar=2)
